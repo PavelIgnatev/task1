23323e5d83130da3da7e0ac1cd5b246ecd672cad
Pavel <palllkaignatev@gmail.com>
2021-03-02 09:44:52 +0300

    build

--- src/renderTemplate/index.js
+++ src/renderTemplate/index.js
@@ -3,7 +3,7 @@ import data from '../data/data.json'
 const renderTemplateAll = (data) => store.commit("renderTemplateAll", data)
 renderTemplateAll(data)
 const renderTemplate = (alias, data) => {store.commit("renderTemplate", [alias, data]);
-return ''}
+return `<div>${alias}</div><div>${data}</div>`}
 window.renderTemplate = renderTemplate
 window.renderTemplateAll = renderTemplateAll
 
