f80aa8c168103e9c3307b356e1a244d5cb3302b4
Pavel <palllkaignatev@gmail.com>
2021-03-05 15:22:20 +0300

    build

--- src/components/rendering/userplace.vue
+++ src/components/rendering/userplace.vue
@@ -54,7 +54,7 @@ export default {
             height: 80%
             bottom: 0
         &:nth-child(2), &:nth-child(4)
-            height: 89%
+            height: 385px
             bottom: 0
             z-index: 100
         &:nth-child(3)
