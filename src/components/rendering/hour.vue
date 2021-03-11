6d1a5e386b6725e2aa60126de4cceb5aa59fa73e
Pavel <palllkaignatev@gmail.com>
2021-03-12 00:51:59 +0300

    build

--- src/components/rendering/hour.vue
+++ src/components/rendering/hour.vue
@@ -1,5 +1,5 @@
 <template>
- <div class="day__hour_wrapper" :style="`${widthX < 568 ? `top: ${15.72 * index}px` : ''}`">
+ <div class="day__hour_wrapper" :style="`${widthX < 568 ? `top: ${16.72 * index}px` : ''}`">
     <div class="day__hour">
         <img :src="require(`@/assets/img/activity/${src}`)"  :style="`z-index: ${index}`">
   </div>
